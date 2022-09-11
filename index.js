15e08a7e4add20d73c17521b4f7cf3552fda03b5
mivoli98 <mivoli98@tamu.edu>
2022-09-11 14:01:31 -0500

    fix serverjs

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 import express from 'express';
-// import bodyParser from 'body-parser';
-// import mongoose from 'mongoose';
 import cors from 'cors';
 
 import dogRoutes from './routes/dog.js';
@@ -9,27 +7,12 @@ import imageRouters from './routes/image.js'
 const app = express();
 const port = 3011;
 
-// app.use(bodyParser.json({ limit: "30mb", extend: true }));
-// app.use(bodyParser.urlencoded({ limit: "30mb", extend: true }));
-// app.use(cors());
 
 app.use(cors({origin: '*'}));
 
 app.use('/', dogRoutes);
 app.use('/breed', imageRouters);
 
-// app.use(async (ctx, next) => {
-// 	await next();
-// 	const rt = ctx.response.get('X-Response-Time');
-// 	console.log(`${ctx.method} ${ctx.url} - ${rt}`);
-// });
-
-// app.use(async (ctx, next) => {
-// 	const start = Date.now();
-// 	await next();
-// 	const ms = Date.now() - start;
-// 	ctx.set('X-Response-Time', `${ms}ms`);
-// });
 
 app.listen(port, () => {
 	console.log(`Server running on http://localhost:${port}`);
